3150618b88c0bb0dfda30c957e42dbff0fe63734
Carl Bedrot <94554211+CarlBedrot@users.noreply.github.com>
2022-09-19 10:50:17 +0000

    Starting with design
    Small start with dsign

--- progg/src/application/Controller.java
+++ progg/src/application/Controller.java
@@ -1,6 +1,11 @@
 package application;
 
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+
+import java.awt.TextField;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -11,7 +16,17 @@ public class Controller {
 	DAL dal = new DAL();
 	
 	@FXML
+	// Consultant 
+	private TabPane tabpane = new TabPane();
 	private TextArea textarea = new TextArea();
+	private TextField TextFieldConsultantName = new TextField();
+	private TextField TextFieldConsultantAddress = new TextField();
+	private TextField TextFieldConsultantStartdate = new TextField();
+	private TextField TextFieldSalary = new TextField();
+	private RadioButton RadioButtonConsultant = new RadioButton();
+	private RadioButton RadioButtonManager = new RadioButton();
+	private AnchorPane AnchorPaneConsultant = new AnchorPane();
+	
 	
 	@FXML
 	private void initialize() {
