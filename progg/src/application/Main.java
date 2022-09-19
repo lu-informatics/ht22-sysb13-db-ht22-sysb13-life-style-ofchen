9d4b78fef03a59514930cb3a7c79d088492991e3
Carl Bedrot <94554211+CarlBedrot@users.noreply.github.com>
2022-09-19 16:27:18 +0000

    Extension
    Commit all

--- progg/src/application/Main.java
+++ progg/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +13,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("View.fxml"));
+			TabPane root = FXMLLoader.load(getClass().getResource("View.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
